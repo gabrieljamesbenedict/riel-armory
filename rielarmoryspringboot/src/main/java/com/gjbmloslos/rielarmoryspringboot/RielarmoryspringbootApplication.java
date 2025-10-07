5034d9b62ff52e263754c33dc258a8e97a1ca0fe
Gabriel James Benedict <benok.loslos@gmail.com>
2025-10-07 12:17:35 +0800

    feat: add springboot

---
+++ rielarmoryspringboot/src/main/java/com/gjbmloslos/rielarmoryspringboot/RielarmoryspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.gjbmloslos.rielarmoryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RielarmoryspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RielarmoryspringbootApplication.class, args);
+	}
+
+}
---
+++ rielarmoryspringboot/src/test/java/com/gjbmloslos/rielarmoryspringboot/RielarmoryspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjbmloslos.rielarmoryspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RielarmoryspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
