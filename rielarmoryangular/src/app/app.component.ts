0b29b368703bb7cfe86c388516c322f8eed9d374
Gabriel James Benedict <benok.loslos@gmail.com>
2025-10-30 13:41:05 +0800

    feat: add home and header components

--- rielarmoryangular/src/app/app.component.ts
+++ rielarmoryangular/src/app/app.component.ts
@@ -3,38 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { ProductService } from './service/product.service';
 import { Product } from './model/product.model';
-import { ManufacturerService } from './service/manufacturer.service';
-import { Manufacturer } from './model/manufacturer.model';
-import { CategoryService } from './service/category.service';
-import { Category } from './model/category.model';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-  title = 'Riel\'s Armory';
+export class AppComponent {
 
   products: Product[] = [];
 
   constructor() {}
 
-  private productService = inject(ProductService);
-
-  ngOnInit(): void {
-    console.log('AppComponent initialized');
-
-    this.productService.getAllProducts().subscribe({
-      next: (data) => {
-        console.log('Products fetched:', data);
-        this.products = data;
-      },
-      error: (err) => console.error('Error fetching products:', err)
-    });
-
-  }
 }
