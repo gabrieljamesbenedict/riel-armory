cb4927f55874c3e69d515eefc98078404398f9bb
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-03 22:58:14 +0800

    feat: add footer component

--- rielarmoryangular/src/app/app.component.ts
+++ rielarmoryangular/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     CommonModule,
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
