d55874f4dd41ff6b6e921a974ed9358be5f7ed52
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-04 16:37:07 +0800

    feat: add store card component

---
+++ rielarmoryangular/src/app/components/store-card/store-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-card',
+  imports: [],
+  templateUrl: './store-card.component.html',
+  styleUrl: './store-card.component.scss'
+})
+export class StoreCardComponent {
+
+}
