ee8d84cf5c2aaa72ac575ae6948de00be57f07b1
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-05 07:50:54 +0800

    feat: add interceptor to all http requests from angular frontend
    Interceptor attaches Autherization header to request to verify auth.

--- rielarmoryangular/src/app/pages/store/store.component.ts
+++ rielarmoryangular/src/app/pages/store/store.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from '../../service/category.service';
+import { ManufacturerService } from '../../service/manufacturer.service';
+
+import { Category } from '../../model/category.model';
+import { Manufacturer } from '../../model/manufacturer.model';
 
 @Component({
   selector: 'app-store',
@@ -6,6 +11,25 @@ import { Component } from '@angular/core';
   templateUrl: './store.component.html',
   styleUrl: './store.component.scss'
 })
-export class StoreComponent {
+export class StoreComponent implements OnInit {
+  
+  private categoryService = inject(CategoryService);
+  private manufacturerService = inject(ManufacturerService);
+
+  public categories: Category[] = []
+  public manufacturers: Manufacturer[] = []
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.categoryService.getAllCategories().subscribe({
+        next: (data) => {
+          this.categories = data;
+        },
+        error: (err) => {
+        console.error('Error fetching categories:', err);
+        }
+      })
+  }
 
 }
--- rielarmoryangular/src/app/service/auth.service.ts
+++ rielarmoryangular/src/app/service/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable, tap } from 'rxjs';
 })
 export class AuthService {
   
-  private apiUrl = 'http://localhost:8080/api/auth';
+  private apiUrl = '/api/auth';
 
   constructor(private http: HttpClient) {}
 
