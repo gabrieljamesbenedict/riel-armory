74fc741aab462cbf4146016f777c1fb0ff704c34
Gabriel James Benedict <benok.loslos@gmail.com>
2025-11-05 12:06:28 +0800

    refactor: match frontend model to backend dto

--- rielarmoryangular/src/app/model/product.model.ts
+++ rielarmoryangular/src/app/model/product.model.ts
@@ -2,13 +2,14 @@ import { Category } from "./category.model";
 import { Manufacturer } from "./manufacturer.model";
 
 export interface Product {
-  productId: number;
+  productId: number; 
   name: string;
   description: string;
   stock: number;
   price: number;
-  manufacturer: Manufacturer;
-  category: Category
-  caliber: number;
+  manufacturerName: string;
+  categoryName: string;
+  tagNames: string[];
+  caliberName: string;
   magazineCapacity: number;
 }
